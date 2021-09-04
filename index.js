5aff170982b8319a625f67f248efd0e937133fff
Kieu <kieuphan530@gmail.com>
2021-09-04 12:59:17 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction(name) {
+    return (`Hi, my name is ${name}.`);
+}
+
+console.log(introduction(`Josh`));
+console.log(introduction(`Nancy`));
+
+function introductionWithLanguage(name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+console.log(introductionWithLanguage(`Josh`, `Ember.js`));
+console.log(introductionWithLanguage(`Nancy`, `React`));
+
+const name = "Gracie";
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
